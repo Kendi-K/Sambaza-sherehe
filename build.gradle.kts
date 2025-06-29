f8b3ae3b97f91f83dd50090af5cc4903ccdebc72
Jesse <jesse.tonui@strathmore.edu>
2025-06-29 23:01:57 +0300

    changed [alias(libs.plugins.kotlin.kapt) apply false] in build.gradle kts changed [kotlin-kapt = { id = "org.jetbrains.kotlin.kapt", version.ref = "kotlin" }] in libs.version.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
+/*plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-}
+}*/
 val roomVersion = "2.6.1"
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     /*id("com.android.application")
     id("org.jetbrains.kotlin.android")*/
-    id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.kotlin.kapt) apply false
 
 }
